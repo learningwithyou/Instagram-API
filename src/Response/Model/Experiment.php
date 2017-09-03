7eb7626f9129b1a764170916691f53aa02e86271
Abyr Valg <valga@abyrga.ru>
2017-09-03 17:40:02 +0300

    A couple of samples for generator output

--- src/Response/Model/Experiment.php
+++ src/Response/Model/Experiment.php
@@ -4,17 +4,6 @@ namespace InstagramAPI\Response\Model;
 
 use InstagramAPI\AutoPropertyHandler;
 
-/**
- * @method mixed getGroup()
- * @method mixed getName()
- * @method Param[] getParams()
- * @method bool isGroup()
- * @method bool isName()
- * @method bool isParams()
- * @method setGroup(mixed $value)
- * @method setName(mixed $value)
- * @method setParams(Param[] $value)
- */
 class Experiment extends AutoPropertyHandler
 {
     /**
@@ -23,4 +12,125 @@ class Experiment extends AutoPropertyHandler
     public $params;
     public $group;
     public $name;
+
+    /**
+     * @return Param[]
+     */
+    public function getParams()
+    {
+        if ($this->params !== null) {
+            return $this->params;
+        } elseif (isset($this->_jsonData->params)) {
+            if (is_array($this->_jsonData->params)) {
+                $this->params = [];
+                foreach ($this->_jsonData->params as $idx => $value) {
+                    $this->params[$idx] = new Param($value);
+                }
+            } else {
+                $this->params = null;
+            }
+
+            return $this->params;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param Param[] $value
+     *
+     * @return static
+     */
+    public function setParams(
+        array $value)
+    {
+        $this->params = $value;
+        $this->_jsonData->params = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isParams()
+    {
+        return isset($this->_jsonData->params) && $this->_jsonData->params;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGroup()
+    {
+        if ($this->group !== null) {
+            return $this->group;
+        } elseif (isset($this->_jsonData->group)) {
+            $this->group = $this->_jsonData->group;
+
+            return $this->group;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return static
+     */
+    public function setGroup(
+        $value)
+    {
+        $this->group = $value;
+        $this->_jsonData->group = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isGroup()
+    {
+        return isset($this->_jsonData->group) && $this->_jsonData->group;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        if ($this->name !== null) {
+            return $this->name;
+        } elseif (isset($this->_jsonData->name)) {
+            $this->name = $this->_jsonData->name;
+
+            return $this->name;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return static
+     */
+    public function setName(
+        $value)
+    {
+        $this->name = $value;
+        $this->_jsonData->name = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isName()
+    {
+        return isset($this->_jsonData->name) && $this->_jsonData->name;
+    }
 }
--- src/Response/SyncResponse.php
+++ src/Response/SyncResponse.php
@@ -6,11 +6,6 @@ use InstagramAPI\AutoPropertyHandler;
 use InstagramAPI\ResponseInterface;
 use InstagramAPI\ResponseTrait;
 
-/**
- * @method Model\Experiment[] getExperiments()
- * @method bool isExperiments()
- * @method setExperiments(Model\Experiment[] $value)
- */
 class SyncResponse extends AutoPropertyHandler implements ResponseInterface
 {
     use ResponseTrait;
@@ -19,4 +14,49 @@ class SyncResponse extends AutoPropertyHandler implements ResponseInterface
      * @var Model\Experiment[]
      */
     public $experiments;
+
+    /**
+     * @return Model\Experiment[]
+     */
+    public function getExperiments()
+    {
+        if ($this->experiments !== null) {
+            return $this->experiments;
+        } elseif (isset($this->_jsonData->experiments)) {
+            if (is_array($this->_jsonData->experiments)) {
+                $this->experiments = [];
+                foreach ($this->_jsonData->experiments as $idx => $value) {
+                    $this->experiments[$idx] = new Model\Experiment($value);
+                }
+            } else {
+                $this->experiments = null;
+            }
+
+            return $this->experiments;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param Model\Experiment[] $value
+     *
+     * @return static
+     */
+    public function setExperiments(
+        array $value)
+    {
+        $this->experiments = $value;
+        $this->_jsonData->experiments = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isExperiments()
+    {
+        return isset($this->_jsonData->experiments) && $this->_jsonData->experiments;
+    }
 }
